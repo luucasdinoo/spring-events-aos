d2671036ce32515032da6fa16ca3cc9bafcdb7e6
Lucas Bernadino <luucasdinoo@gmail.com>
2024-10-09 13:19:52 -0300

    First commit

---
+++ src/main/java/br/com/event/events/SpringEventsAosApplication.java
@@ -0,0 +1,13 @@
+package br.com.event.events;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEventsAosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEventsAosApplication.class, args);
+    }
+
+}
